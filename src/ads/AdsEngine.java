2748402370e01575993723e94315fd175aa453a1
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-14 21:43:56 -0700

    ads data input handling done

--- src/ads/AdsEngine.java
+++ src/ads/AdsEngine.java
@@ -1,16 +1,19 @@
 package ads;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by zzc on 8/14/17.
  */
+
 public class AdsEngine {
     private String mAdsDataFilePath;
     private String mBudgetFilePath;
@@ -22,7 +25,8 @@ public class AdsEngine {
     private String mysql_user;
     private String mysql_pass;
 
-    public AdsEngine(String mAdsDataFilePath, String mBudgetFilePath, String mMemcachedServer, int mMemcachedPortal, String mysql_host, String mysql_db, String mysql_user, String mysql_pass) {
+    public AdsEngine(String mAdsDataFilePath, String mBudgetFilePath, String mMemcachedServer,
+                     int mMemcachedPortal, String mysql_host, String mysql_db, String mysql_user, String mysql_pass) {
         this.mAdsDataFilePath = mAdsDataFilePath;
         this.mBudgetFilePath = mBudgetFilePath;
         this.mMemcachedServer = mMemcachedServer;
@@ -34,29 +38,34 @@ public class AdsEngine {
         indexBuilder = new IndexBuilder();
     }
 
-    public boolean initEngine() {
+    public void initEngine() {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(mAdsDataFilePath));
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                JSONObject jsonObject = new JSONObject();
+                JSONObject jsonObject = new JSONObject(line);
+
                 if (jsonObject.isNull("adId") || jsonObject.isNull("campaignId")) {
                     continue;
                 }
                 long adId = jsonObject.getLong("adId");
                 long campaignId = jsonObject.getLong("campaignId");
                 String brand = jsonObject.isNull("brand") ? "" : jsonObject.getString("brand");
-                String price = jsonObject.isNull("price") ? "" : jsonObject.getString("price");
+                double price = jsonObject.isNull("price") ? 100.0 : jsonObject.getDouble("price");
                 String thumbnail= jsonObject.isNull("thumbnail") ? "" : jsonObject.getString("thumbnail");
                 String title = jsonObject.isNull("title") ? "" : jsonObject.getString("title");
                 String detailUrl= jsonObject.isNull("detail_url") ? "" : jsonObject.getString("detail_url");
-                String bidPrice = jsonObject.isNull("bidPrice") ? "" : jsonObject.getString("bidPrice");
-                String pClick = jsonObject.isNull("pClick") ? "" : jsonObject.getString("pClick");
+                double bidPrice = jsonObject.isNull("bidPrice") ? 1.0 : jsonObject.getDouble("bidPrice");
+                double pClick = jsonObject.isNull("pClick") ? 0.0 : jsonObject.getDouble("pClick");
                 String category = jsonObject.isNull("category") ? "" : jsonObject.getString("category");
                 String description = jsonObject.isNull("description") ? "" : jsonObject.getString("description");
-                String keyWords = jsonObject.isNull("keyWords") ? "" : jsonObject.getString("keyWords");
-                List<String>
-                Ad ad = new Ad()
+                JSONArray keyWords = jsonObject.isNull("keyWords") ? null : jsonObject.getJSONArray("keyWords");
+
+                List<String> keyWordsList = new ArrayList<String>();
+                for (Object keyWord : keyWords) {
+                    keyWordsList.add(String.valueOf(keyWord));
+                }
+//                Ad ad = new Ad()
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
--- src/driver/Main.java
+++ src/driver/Main.java
@@ -28,28 +28,30 @@ public class Main {
         String memcachedServer = args[2];
         int memcachedPortal = Integer.parseInt(args[3]);
 
-        AdsEngine adsEngine = new AdsEngine(adsDataFilePath, budgetDataFilePath, memcachedServer, memcachedPortal, MYSQL_HOST_PORT, MYSQL_DB, MYSQL_USER, MYSQL_PASS);
-        if(adsEngine.init())
-        {
-            System.out.println("Ready to take quey");
-            try{
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                String query;
-                while((query=br.readLine())!=null){
-                    //System.out.println(query);
-                    List<Ad> adsCandidates = adsEngine.selectAds(query);
-                    for(Ad ad : adsCandidates)
-                    {
-                        System.out.println("final selected ad id = " + ad.adId);
-                        System.out.println("final selected ad rank score = " + ad.rankScore);
-                    }
-                }
-
-            }catch(IOException io){
-                io.printStackTrace();
-            }
-        } else {
-            System.err.println("Ads Engine initilize failed!");
-        }
+        AdsEngine adsEngine = new AdsEngine(adsDataFilePath, budgetDataFilePath, memcachedServer,
+                memcachedPortal, MYSQL_HOST_PORT, MYSQL_DB, MYSQL_USER, MYSQL_PASS);
+        adsEngine.initEngine();
+//        if(adsEngine.init())
+//        {
+//            System.out.println("Ready to take quey");
+//            try{
+//                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//                String query;
+//                while((query=br.readLine())!=null){
+//                    System.out.println(query);
+//                    List<Ad> adsCandidates = adsEngine.selectAds(query);
+//                    for(Ad ad : adsCandidates)
+//                    {
+//                        System.out.println("final selected ad id = " + ad.adId);
+//                        System.out.println("final selected ad rank score = " + ad.rankScore);
+//                    }
+//                }
+//
+//            }catch(IOException io){
+//                io.printStackTrace();
+//            }
+//        } else {
+//            System.err.println("Ads Engine initilize failed!");
+//        }
     }
 }
